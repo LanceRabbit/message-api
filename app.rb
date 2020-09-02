cdfa1f4a42eaacba5a9be32893656c749e5e1d02
Lancerabbit <Lancerabbit@gmail>
2020-09-02 18:21:50 +0800

    callback api work

---
+++ app.rb
@@ -0,0 +1,44 @@
+# app.rb
+require 'sinatra'
+require 'line/bot'
+require 'dotenv/load'
+# require 'pry'
+
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_id = ENV["LINE_CHANNEL_ID"]
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+end
+
+post '/callback' do
+  body = request.body.read
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+  puts 'validate_signature is pass'
+  events = client.parse_events_from(body)
+  events.each do |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        puts "msg: #{event.message['text']}"
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
+        client.reply_message(event['replyToken'], message)
+      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+        response = client.get_message_content(event.message['id'])
+        tf = Tempfile.open("content")
+        tf.write(response.body)
+      end
+    end
+  end
+
+  # Don't forget to return a successful response
+  "OK"
+end
